2bbf7a5beea548e48908bbaf67b87cbc06b7fe22
duyhung753951 <148344213+duyhung753951@users.noreply.github.com>
2024-04-01 13:25:25 +0700

    Create Bai_2.cpp

---
+++ Bai_2.cpp
@@ -0,0 +1,38 @@
+//tinh bcnn(a,b), ap dung ham tren de tinh bcnn cua 1 day so nguyen bt bcnn(a, b) = a*b/ucln(a, b)
+#include<iostream>
+using namespace std;
+int bcnn(int a, int b);
+int ucln(int a, int b);
+void input_arr(int n, int a[]);
+int main(){
+    int n, tmp;
+    cin >>  n;
+    int a[n];
+    input_arr(n, a);
+    tmp = bcnn(a[0], a[1]);
+    for(int i = 2; i < n; i++){
+        tmp = bcnn(tmp, a[i]);
+    }
+    cout << "ket qua = " << tmp;
+    return 0;
+}
+
+int bcnn(int a, int b)
+{
+    return (a*b)/ucln(a, b);
+}
+
+int ucln(int a, int b)
+{
+    if(b == 0){
+        return a;
+    }
+    return ucln(b, a % b);
+}
+
+void input_arr(int n, int a[])
+{
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+}
